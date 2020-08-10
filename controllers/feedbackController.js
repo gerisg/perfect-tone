5e1abdd1e9b96a208910f98d7887e366c2356730
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-08-10 14:57:22 -0300

    Add result

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -5,9 +5,18 @@ function save(feedback) {
     const filename = path.join(__dirname, '/../data/feedback.json');
     let fileContent = fs.readFileSync(filename, 'utf8');
     let currentFeedback = JSON.parse(fileContent);
-    let newRegister = { q1: feedback.q1,q2: feedback.q2,q3: feedback.q3,q4: feedback.q4,q5: feedback.q5, accept: feedback.agree, comment: feedback.comment }
+    let newRegister = {
+        q1: feedback.q1,
+        q2: feedback.q2,
+        q3: feedback.q3,
+        q4: feedback.q4,
+        q5: feedback.q5,
+        products: feedback.products,
+        accept: feedback.agree,
+        comment: feedback.comment
+    }
     currentFeedback.push(newRegister);
-    fs.writeFileSync(filename, JSON.stringify(currentFeedback));
+    fs.writeFileSync(filename, JSON.stringify(currentFeedback, null, 1));
 }
 
 module.exports = {
